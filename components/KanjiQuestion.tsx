3f08d94d76be325e3ceec60e82a0ad22224c2a39
toshinori sato <tyahhahha@gmail.com>
2023-08-21 15:20:04 +0900

    履歴をコンポーネントに切り出し

--- components/KanjiQuestion.tsx
+++ components/KanjiQuestion.tsx
@@ -1,8 +1,8 @@
 import { Kanji } from "@/data/kanji"
 import { useMemo, useState } from "react"
-import { getHistory, saveHistory, saveHistoryAtReview } from "@/logics/history"
-import dayjs from "dayjs"
+import { saveHistory, saveHistoryAtReview } from "@/logics/history"
 import { useAppContext } from "@/context"
+import { Histories } from "@/components/Histories"
 
 type Props = {
   data: Kanji
@@ -35,7 +35,6 @@ export const KanjiQuestion = ({ data, onPrev, onNext, onReturnTitle }: Props) =>
     onNext()
   }
 
-  const history = useMemo(() => getHistory(data).sort((a, b) => b.datetime - a.datetime), [data])
   const { questions } = useAppContext()
   const index = useMemo(() => questions.findIndex((q) => q.id === data.id), [data, questions])
 
@@ -109,48 +108,7 @@ export const KanjiQuestion = ({ data, onPrev, onNext, onReturnTitle }: Props) =>
           </button>
         </div>
         <div className="relative overflow-x-auto shadow-md mt-8 w-fit m-auto min-w-8">
-          <table className="text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-3">
-                  履歴
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  ◎✖
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {history.map((h) => {
-                return (
-                  <tr
-                    key={h.datetime}
-                    className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
-                  >
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      {dayjs(h.datetime).format("YYYY/MM/DD(ddd)")}
-                    </th>
-                    <td className="px-6 py-4">{h.isCorrect ? "◎" : "✖"}</td>
-                  </tr>
-                )
-              })}
-              <tr>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  合計
-                </th>
-                <td className="px-6 py-4">
-                  ◎：{history.filter((h) => h.isCorrect).length}, ✖：
-                  {history.filter((h) => !h.isCorrect).length}
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <Histories kanji={data} />
         </div>
       </section>
     </main>
