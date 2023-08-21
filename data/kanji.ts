f27ecce2e5c2bb833d5d53b5936c65cdf327432d
toshinori sato <tyahhahha@gmail.com>
2023-08-21 13:25:11 +0900

    漢字データ４追加

--- data/kanji.ts
+++ data/kanji.ts
@@ -1,6 +1,7 @@
 import { KanjiData1 } from "@/data/kanji001"
 import { KanjiData2 } from "@/data/kanji002"
 import { KanjiData3 } from "@/data/kanji003"
+import { KanjiData4 } from "@/data/kanji004"
 
 type QuestionType = "read" | "write"
 
@@ -12,4 +13,4 @@ export type Kanji = {
   questionType: QuestionType
 }
 
-export const KanjiData: Kanji[] = [...KanjiData1, ...KanjiData2, ...KanjiData3]
+export const KanjiData: Kanji[] = [...KanjiData1, ...KanjiData2, ...KanjiData3, ...KanjiData4]
