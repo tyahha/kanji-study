d8f2fa73f72ffdf21a9311c3bb93dde57698818f
toshinori sato <tyahhahha@gmail.com>
2023-08-22 09:07:52 +0900

    漢字データ追加

--- data/kanji.ts
+++ data/kanji.ts
@@ -5,6 +5,11 @@ import { KanjiData4, KanjiData4Category } from "@/data/kanji004"
 import { KanjiDataB1_2, KanjiDataB1_2Category } from "@/data/kanjiB001-2"
 import { KanjiDataB1_1, KanjiDataB1_1Category } from "@/data/kanjiB001-1"
 import { KanjiData5, KanjiData5Category } from "@/data/kanji005"
+import { KanjiData6, KanjiData6Category } from "@/data/kanji006"
+import { KanjiData7, KanjiData7Category } from "@/data/kanji007"
+import { KanjiData8, KanjiData8Category } from "@/data/kanji008"
+import { KanjiDataB2_1, KanjiDataB2_1Category } from "@/data/kanjiB002-1"
+import { KanjiDataB2_2, KanjiDataB2_2Category } from "@/data/kanjiB002-2"
 
 type QuestionType = "read" | "write"
 
@@ -24,6 +29,11 @@ export const KanjiDataCategories = [
   KanjiDataB1_1Category,
   KanjiDataB1_2Category,
   KanjiData5Category,
+  KanjiData6Category,
+  KanjiData7Category,
+  KanjiData8Category,
+  KanjiDataB2_1Category,
+  KanjiDataB2_2Category,
 ]
 
 export const KanjiData: Kanji[] = [
@@ -34,4 +44,9 @@ export const KanjiData: Kanji[] = [
   ...KanjiDataB1_1,
   ...KanjiDataB1_2,
   ...KanjiData5,
+  ...KanjiData6,
+  ...KanjiData7,
+  ...KanjiData8,
+  ...KanjiDataB2_1,
+  ...KanjiDataB2_2,
 ]
